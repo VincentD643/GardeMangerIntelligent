51e78549170db555d283c867733035d7a80600a2
Ryuketsuki <jimmy.mai.1@ens.etsmtl.ca>
2022-02-18 17:56:02 -0500

    Local push notification avec react-native-push-notification

--- components/Notification/Notification.js
+++ components/Notification/Notification.js
@@ -1,114 +1,69 @@
-//import {useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import React, {useState, useEffect, useRef} from 'react';
 import {Text, View, Button, Platform} from 'react-native';
-import Constants from 'expo-constants'
-import * as Notifications from 'expo-notifications';
+import PushNotification from "react-native-push-notification";
 
-Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-        shouldShowAlert: true,
-        shouldPlaySound: false,
-        shouldSetBadge: false,
-    }),
+// Must be outside of any component LifeCycle (such as `componentDidMount`).
+PushNotification.configure({
+
+    // (required) Called when a remote is received or opened, or local notification is opened
+    onNotification: function (notification) {
+        console.log("NOTIFICATION:", notification);
+        requestPermissions: Platform.OS === 'android'
+    },
+
+    requestPermissions: true,
 });
-const Notification = () => {
 
-    const [expoPushToken, setExpoPushToken] = useState('');
-    const [notification, setNotification] = useState(false);
-    const notificationListener = useRef();
-    const responseListener = useRef();
+const Notification = () => {
 
 
     useEffect(() => {
+        PushNotification.createChannel(
+            {
+                channelId: "gardeMangerChannel",
+                channelName: "Garde Manger Channel"
+            }
+        )
+    }, [])
 
-        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-
-        notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
-            setNotification(notification);
-            console.log(notification)
-        });
-
-        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-            console.log(response);
-        });
-
-        return () => {
-            Notifications.removeNotificationSubscription(notificationListener.current);
-            Notifications.removeNotificationSubscription(responseListener.current);
-        };
-    }, []);
-
-    const message = getExpirationDate(expoPushToken)
     return (
         <View>
-            <Text>Your expo push token: {expoPushToken}</Text>
-            <View style={{alignItems: 'center', justifyContent: 'center'}}>
-                <Text>Title: {notification && notification.request.content.title} </Text>
-                <Text>Body: {notification && notification.request.content.body}</Text>
-                <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
-            </View>
             <Button
                 title="Press to Send Notification"
                 onPress={async () => {
-                    await sendPushNotification(expoPushToken,message);
-                    //await schedulePushNotification();
+                    await pushNotification()
                 }}
             />
         </View>
     )
 }
 
+async function pushNotification() {
+    //const expirationArray = getExpirationDate()
+    for (let i = 0; i < 2; i++) {
+        PushNotification.localNotification(
+            {
+                channelId: "gardeMangerChannel",
+                title: "New Update",
+                message: "Local test" + i
+            }
+        )
+    }
+}
 
-function getExpirationDate(expoPushToken){
-    return{
+
+function getExpirationDate() {
+
+    return {
         sound: 'default',
         title: 'sdgsdhx',
         body: 'And dsgsdgsdgs!',
         data: {someData: 'asfa hasfasfgasgere'},
-        channelId:'default',
-        priority:'high',
+        channelId: 'default',
+        priority: 'high',
         to: expoPushToken,
     }
 }
-
-// Can use this function below, OR use Expo's Push Notification Tool-> https://expo.dev/notifications
-async function sendPushNotification(expoPushToken,message) {
-
-    await fetch('https://exp.host/--/api/v2/push/send', {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Accept-encoding': 'gzip, deflate',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(message),
-    });
-    // console.log(JSON.stringify(message))
-}
-
-async function registerForPushNotificationsAsync() {
-    let token;
-    if (!Constants.isDevice) {
-        return Promise.reject('Must use physical device for Push Notifications');
-    }
-
-    const status = (await Notifications.requestPermissionsAsync()).status
-    if (status !== "granted") {
-        return Promise.reject("Failed to push token")
-    }
-    if (Platform.OS === 'android') {
-        await Notifications.setNotificationChannelAsync('default', {
-            name: 'default',
-            showBadge: true,
-            importance: Notifications.AndroidImportance.MAX,
-            vibrationPattern: [0, 250, 250, 250],
-            lightColor: '#FF231F7C',
-        });
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data
-    console.log(token)
-    return token;
-}
-
 
 export default Notification;
