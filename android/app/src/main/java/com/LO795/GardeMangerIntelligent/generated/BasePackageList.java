a56d10d943e63658dcc91b8f9f452fee078b0b48
Ryuketsuki <jimmy.mai.1@ens.etsmtl.ca>
2022-02-21 19:51:28 -0500

    Small fix

--- android/app/src/main/java/com/LO795/GardeMangerIntelligent/generated/BasePackageList.java
+++ android/app/src/main/java/com/LO795/GardeMangerIntelligent/generated/BasePackageList.java
@@ -11,13 +11,11 @@ public class BasePackageList {
         new expo.modules.barcodescanner.BarCodeScannerPackage(),
         new expo.modules.camera.CameraPackage(),
         new expo.modules.constants.ConstantsPackage(),
-        new expo.modules.device.DevicePackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
         new expo.modules.imageloader.ImageLoaderPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
-        new expo.modules.notifications.NotificationsPackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.updates.UpdatesPackage()
     );
